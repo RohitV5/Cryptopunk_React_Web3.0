ba5a422964ee49f26be980b74ef4ab334c224520
Rohit Verma <verma.rohit.in@gmail.com>
2021-12-13 07:26:14 +0530

    complete header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,41 @@
 import {React} from 'react';
 import './Header.css';
 import punkLogo from '../assets/header/cryptopunk-logo.png';
-import searchIcon from '../assets/header/search.png'
+import searchIcon from '../assets/header/search.png';
+import themeSwitchIcon from '../assets/header/theme-switch.png';
 
 function Header() {
     return (
         <div className="header">
 
             <div className='logoContainer'>
-                <img src={punkLogo}  className='punkLogo' alt='' />
+                <img src={punkLogo}  className='punkLogo' alt='punkLogo' />
             </div>
 
             <div className='searchBar'>
                 <div className='searchIconContainer'>
-                    <img src={searchIcon}></img>
+                    <img src={searchIcon} alt='search-icon'></img>
                 </div>
                 <input className='searchInput' placeholder='Collections, item or user...' />
+            </div>
+
+            <div className='headerItems'>
+                <p>Drops</p>
+                <p>Marketplace</p>
+                <p>Create</p>                
+            </div>
+
+            <div className='headerActions'>
+                <div className='themeSwitchContainer'>
+                    <img src={themeSwitchIcon} alt='theme-switch-icon' />
+                </div>
+                <div>
+
+                </div>
+            </div>
+
+            <div className='loginButton'>
+                GET IN
             </div>
 
         </div>
